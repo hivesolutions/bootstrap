4762719aa40ec4723a815a3a74eafb83b416c77b
João Magalhães <joamag@gmail.com>
2012-09-04 15:57:49 +0100

    new project structure

--- win32/update.bat
+++ win32/update.bat
@@ -1,22 +1,22 @@
-:: Hive Colony Framework
+:: Hive Bootstrap Scripts
 :: Copyright (C) 2008-2012 Hive Solutions Lda.
 ::
-:: This file is part of Hive Colony Framework.
+:: This file is part of Hive Bootstrap Scripts.
 ::
-:: Hive Colony Framework is free software: you can redistribute it and/or modify
+:: Hive Bootstrap Scripts is free software: you can redistribute it and/or modify
 :: it under the terms of the GNU General Public License as published by
 :: the Free Software Foundation, either version 3 of the License, or
 :: (at your option) any later version.
 ::
-:: Hive Colony Framework is distributed in the hope that it will be useful,
+:: Hive Bootstrap Scripts is distributed in the hope that it will be useful,
 :: but WITHOUT ANY WARRANTY; without even the implied warranty of
 :: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 :: GNU General Public License for more details.
 ::
 :: You should have received a copy of the GNU General Public License
-:: along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+:: along with Hive Bootstrap Scripts. If not, see <http://www.gnu.org/licenses/>.
 
-:: __author__    = Jo„o Magalh„es <joamag@hive.pt>
+:: __author__    = Jo√£o Magalh√£es <joamag@hive.pt>
 :: __version__   = 1.0.0
 :: __revision__  = $LastChangedRevision$
 :: __date__      = $LastChangedDate$
@@ -26,35 +26,10 @@
 :: turns off the echo
 @echo off
 
-:: prints an information message
-echo Updating the hive repositories ...
+:: executes the bootstrap script after
+:: the download of the current version
+python ../lib/bootstrap.py --download
+python ../lib/bootstrap.py --update
 
-:: cloning the various colony repositories
-cd colony && git pull && git submodule foreach git pull
-cd ..
-cd colony_plugins && git pull && git submodule foreach git pull
-cd ..
-cd colony_config && git pull && git submodule foreach git pull
-cd ..
-cd omni && git pull && git submodule foreach git pull
-cd ..
-
-:: cloning the ui external libraries
-cd uxf && git pull && git submodule foreach git pull
-cd ..
-cd uxf_bin && git pull && git submodule foreach git pull
-cd ..
-
-:: cloning the varius extra plugins
-cd hive_site && git pull && git submodule foreach git pull
-cd ..
-cd hive_blog && git pull && git submodule foreach git pull
-cd ..
-
-:: cloning the misc projects
-cd dns_registers && git pull && git submodule foreach git pull
-cd ..
-
-:: cloning the viriatum projects
-cd viriatum && git pull && git submodule foreach git pull
-cd ..
+:: exits the process
+exit /b %ERRORLEVEL%
