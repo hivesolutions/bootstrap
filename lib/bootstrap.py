414d1aab393d6993765a3dd2c059225b6afce1fb
João Magalhães <joamag@gmail.com>
2019-06-01 10:29:31 +0100

    New repo naming

--- lib/bootstrap.py
+++ lib/bootstrap.py
@@ -351,7 +351,7 @@ REPOSITORIES = {
         "ripe-retail",
         "ripe-retail-vendors",
         "ripe-robin",
-        "ripe-rtcsr",
+        "ripe-csr",
         "ripe-runner",
         "ripe-sales",
         "ripe-sap",
